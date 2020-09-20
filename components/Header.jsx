ca3403ab0de6615fbe3150b1895c6b1207a32363
Elton Jothi <elton.jothi@edgeprop.my>
2020-09-20 16:14:44 +0800

    added home link to header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,18 +1,22 @@
 import { css } from "@emotion/css";
+import Link from "next/link";
 import tw from "@tailwindcssinjs/macro";
 
 const Header = () => (
   <>
     <div className={css(tw`container mx-auto`)}>
-      <h1
-        className={css`
-          ${tw`text-ha font-extrabold text-2xl md:text-4xl text-center mt-4 mb-4`}
-          /* color: #fb6f0d; */
+      <Link href="/">
+        <a>
+          <h1
+            className={css`
+              ${tw`text-ha font-extrabold text-2xl md:text-4xl text-center mt-4 mb-4`}/* color: #fb6f0d; */
           /* font-size: 24px; */
-        `}
-      >
-        The Rick and Morty API
-      </h1>
+            `}
+          >
+            The Rick and Morty API
+          </h1>
+        </a>
+      </Link>
     </div>
   </>
 );
